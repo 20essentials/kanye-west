681a9851bb1336301a5fb2cfa5cc68b821861f0e
DominicDev <s0l0estudi4r@gmail.com>
2025-11-02 13:57:38 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'KANYE WEST ';
+export let nameSongs = [
+	"FLASHING LIGHTS (ALTERNATE INTRO)", /*1*/
+	"POWER", /*2*/
+	"DARK FANTASY", /*3*/
+	"HEARTLESS", /*4*/
+	"RUNAWAY", /*5*/
+	"MOON", /*6*/
+	"STRONGER", /*7*/
+	"GOLD DIGGER", /*8*/
+	"ALL OF THE LIGHTS", /*9*/
+	"FLASHING LIGHTS ", /*10*/
+	"I WONDER", /*11*/
+	"CAN'T TELL ME NOTHING", /*12*/
+	"BLACK SKINHEAD", /*13*/
+	"BOUND 2", /*14*/
+	"JESUS WALKS", /*15*/
+	"GOOD MORNING", /*16*/
+	"ULTRALIGHT BEAM", /*17*/
+	"SLOW JAMZ", /*18*/
+	"ALL FALLS DOWN", /*19*/
+	"THROUGH THE WIRE", /*20*/
+]
